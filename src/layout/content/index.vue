156fc3cc50de309740cca4a3b0cd5a06d04b8c7f
hehepeng <cnmsbqnm@gmail.com>
2023-05-05 23:37:47 +0800

    开发页头

--- src/layout/content/index.vue
+++ src/layout/content/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <el-main>
+  <el-main class="main-content">
     <router-view />
   </el-main>
 </template>
@@ -9,3 +9,13 @@ export default {
   name: 'MainContent'
 }
 </script>
+
+<style lang="scss" scoped>
+.main-content {
+  min-height: 100vh;
+  max-width: 1200px;
+  position: relative;
+  margin: 0 auto;
+  width: 100%;
+}
+</style>
--- src/layout/header/index.vue
+++ src/layout/header/index.vue
@@ -1,18 +1,44 @@
 <template>
-  <el-header class="nav-header">
-    <section class="left-box">
-      <span class="nav-item"
-            :class="[currentIndex === index ? 'is-active' : '']"
-            v-for="(item, index) of navs"
-            :key="index"
-            @click="toTarget(item, index)">
-        {{ item.label }}
-        <span class="border-line"
-              :class="[currentIndex === index ? 'is-active' : '']"></span>
-      </span>
+  <el-header
+             class="nav-header flex-row main-between">
+    <section
+             class="container flex-row main-between">
+      <section
+               class="left-box flex-row secondary-center">
+        <span class="nav-item flex-inline main-center secondary-center"
+              :class="[currentIndex === index ? 'is-active' : '']"
+              v-for="(item, index) of navs"
+              :key="index"
+              @click="toTarget(item, index)">
+          {{ item.label }}
+          <span class="border-line"
+                :class="[currentIndex === index ? 'is-active' : '']"></span>
+        </span>
+      </section>
+
+      <section
+               class="right-box flex-row secondary-center">
+        <svg-icon icon-class="notice" />
+        <el-button class="login-btn"
+                   type="primary" plain
+                   size="medium" @click="login">登录
+          |
+          注册</el-button>
+      </section>
     </section>
 
-    <section class="right-box"></section>
+    <el-dialog :visible.sync="loginVisible"
+               title="登录享受更多权益" append-to-body
+               :close-on-click-modal="false">
+      <span>这是一段信息</span>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="loginVisible = false">取
+          消</el-button>
+        <el-button type="primary"
+                   @click="loginVisible = false">确
+          定</el-button>
+      </span>
+    </el-dialog>
   </el-header>
 </template>
 
@@ -34,7 +60,8 @@ export default {
         label: '活动',
         name: 'activity'
       }],
-      currentIndex: -1
+      currentIndex: -1,
+      loginVisible: false
     }
   },
   methods: {
@@ -43,6 +70,9 @@ export default {
       this.$router.push({
         name: item.name
       })
+    },
+    login () {
+      this.loginVisible = true
     }
   }
 }
@@ -50,39 +80,66 @@ export default {
 
 <style lang="scss" scoped>
 .nav-header {
-  display: flex;
-  align-items: center;
+  width: 100%;
+  background: #fff;
+  border-bottom: 1px solid #f1f1f1;
+  color: #909090;
+  z-index: 250;
+
+  position: sticky;
+  top: 0;
+  left: 0;
+  right: 0;
+  transition: all 0.2s;
+}
+.container {
   max-width: 1440px;
   margin: auto;
-  .nav-item {
-    min-width: 80px;
-    box-sizing: border-box;
-    padding: 5px 10px;
-    color: #909399;
-    text-align: center;
-    font-size: 14px;
-    position: relative;
-    &:hover {
-      color: #1e80ff;
+  position: relative;
+  width: 100%;
+  height: 100%;
+  .left-box {
+    height: 100%;
+    .nav-item {
+      min-width: 80px;
+      height: 100%;
+      box-sizing: border-box;
+      color: #909399;
+      text-align: center;
+      font-size: 14px;
+      position: relative;
+      &:hover {
+        color: #1e80ff;
+        cursor: pointer;
+      }
+      &.is-active {
+        color: #1e80ff;
+      }
+      .border-line {
+        display: inline-block;
+        width: 0;
+        height: 1px;
+        background: transparent;
+        transition: width 0.63s ease;
+        position: absolute;
+        top: 100%;
+        left: -50%;
+        transform: translateX(50%);
+        &.is-active {
+          width: 100%;
+          background-color: #1e80ff;
+        }
+      }
+    }
+  }
+  .right-box {
+    height: 100%;
+    .svg-icon {
+      font-size: 24px;
       cursor: pointer;
     }
-    &.is-active {
-      color: #1e80ff;
-    }
-    .border-line {
-      display: inline-block;
-      width: 0;
-      height: 1px;
-      background: transparent;
-      transition: width 0.3s ease;
-      position: absolute;
-      top: 100%;
-      left: -50%;
-      transform: translateX(50%);
-      &.is-active {
-        width: 100%;
-        background-color: #1e80ff;
-      }
+    .login-btn {
+      margin-left: 10px;
     }
   }
 }

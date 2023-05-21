8833e3b7805954701292dcf07559ee74c87e5c5c
hehepeng <cnmsbqnm@gmail.com>
2023-05-21 16:37:39 +0800

    修改代理地址

--- config/index.js
+++ config/index.js
@@ -4,12 +4,12 @@ module.exports = {
   port: 3000,
   dev: {
     proxyTable: {
-      '/mall-service': {
-        target: 'http://localhost:8088/mall-service',
+      '/user-management-service': {
+        target: 'http://localhost:8088/user-management-service',
         secure: false, // 如果是https接口，需要配置该参数，表示是否校验证书，开发环境改为false
         changeOrigin: true,
         pathRewrite: {
-          '^/mall-service': ''
+          '^/user-management-service': ''
         }
       }
     }

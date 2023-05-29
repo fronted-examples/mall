e71f9269498dabe06f661b5787e75a23ecea8173
hehepeng <cnmsbqnm@gmail.com>
2023-05-29 23:00:49 +0800

    将部分依赖迁移到devDependencies中

--- src/store/index.js
+++ src/store/index.js
@@ -7,28 +7,6 @@ import user from './user/index'
 
 Vue.use(Vuex)
 
-// 存储服务端vuex前，将window对象绑定到global上
-// if (process.env.VUE_ENV === "server") {
-//     require('@/utils/server-add-window.js')
-//     require('@/utils/flexible')
-// }
-
-/**
- * 因为服务端渲染会每次都创建一个新的store
- * 所以不做持续化缓存
- */
-// const appPersistedState = createPersistedState({
-//     key: 'app',
-//     storage: global.window.sessionStorage, // 修改存储的状态
-//     paths: ['app'] // 存储的指定的模块的名字（存储某个模块对象）
-// })
-
-// const userPersistedState = createPersistedState({
-//     key: 'user',
-//     storage: global.window.sessionStorage, // 修改存储的状态
-//     paths: ['user'] // 存储的指定的模块的名字（存储某个模块对象）
-// })
-
 export const createStore = () => {
     return new Vuex.Store({
         modules: {
